3bffad50f449afd3322a89db25db849028c7cfc0
Afimc <afimc1912@gmail.com>
2024-04-08 18:23:21 +0100

    basic server and front-end home page + some css

--- chess-app/src/App.tsx
+++ chess-app/src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.scss'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+
+const App = ()=> {
+  return(
+    <div className="App">
+      <input id='NickNameInput' type="text"  placeholder='Username'></input>
+      <div className="buttons">
+        <button>Random Game</button>
+        <button>Game with friend</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="waitingList">
+        <ol>
+          <li>Miro</li>
+          <li>valio</li>
+        </ol>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
+
 
 export default App
