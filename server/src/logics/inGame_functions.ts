c2f8c1a7cc736dc71f03eef7cff4c846cf288b1b
Afimc <afimc1912@gmail.com>
2024-04-17 09:22:56 +0100

    pice movements

--- server/src/logics/inGame_functions.ts
+++ server/src/logics/inGame_functions.ts
@@ -1,3 +1,5 @@
+import { Piece } from "./test";
+
 export enum EPiece {
   PAWN = "PAWN",
   ROOK = "TOP",
@@ -5,6 +7,15 @@ export enum EPiece {
   BISHOP = "BISHOP",
   QUEEN = "QUEEN",
   KING = "KING",
+}
+
+export enum EColor {
+  BLACK,
+  WHITE,
+}
+export interface Iposition {
+  x: number,
+  y: number,
 }
 
 export function positionConvertToVector(position: string) {
@@ -24,109 +35,27 @@ export function positionConvertToString(vector: { x: number; y: number }) {
   return position;
 }
 
-export function availablePositions(
-  pos: { x: number; y: number },
-  type: EPiece
-) {
-  const { x, y } = pos;
-  if (type === EPiece.ROOK) {
-    return rookPositions(x, y);
-  } else if (type === EPiece.KNIGHT) {
-    return knightPositions(x, y);
-  } else if (type === EPiece.BISHOP) {
-    return bishopPositions(x, y);
-  }
-}
 
-export function rookPositions(x:number, y:number) {
-  const rookPositions = [
-    { x: x + 1, y: y },
-    { x: x, y: y + 1 },
-    { x: x + 2, y: y },
-    { x: x, y: y + 2 },
-    { x: x + 3, y: y },
-    { x: x, y: y + 3 },
-    { x: x + 4, y: y },
-    { x: x, y: y + 4 },
-    { x: x + 5, y: y },
-    { x: x, y: y + 5 },
-    { x: x + 6, y: y },
-    { x: x, y: y + 6 },
-    { x: x + 7, y: y },
-    { x: x, y: y + 7 },
-    { x: x - 7, y: y },
-    { x: x, y: y - 7 },
-    { x: x - 6, y: y },
-    { x: x, y: y - 6 },
-    { x: x - 5, y: y },
-    { x: x, y: y - 5 },
-    { x: x - 4, y: y },
-    { x: x, y: y - 4 },
-    { x: x - 3, y: y },
-    { x: x, y: y - 3 },
-    { x: x - 2, y: y },
-    { x: x, y: y - 2 },
-    { x: x - 1, y: y },
-    { x: x, y: y - 1 },
-  ];
-  const filteredRookPositions = rookPositions.filter(
-    (pos) => pos.x >= 0 && pos.y >= 0 && pos.x < 8 && pos.y < 8
-  );
-  return filteredRookPositions;
-}
 
-export function knightPositions(x:number, y:number) {
-  const knightPositions = [
-    { x: x - 2, y: y + 1 },
-    { x: x - 2, y: y - 1 },
-    { x: x + 2, y: y + 1 },
-    { x: x + 2, y: y - 1 },
-
-    { x: x - 1, y: y + 2 },
-    { x: x - 1, y: y - 2 },
-    { x: x + 1, y: y + 2 },
-    { x: x + 1, y: y - 2 },
-  ];
-  const filteredKnightPositions = knightPositions.filter(
-    (pos) => pos.x >= 0 && pos.y >= 0 && pos.x < 8 && pos.y < 8
-  );
-  return filteredKnightPositions;
-}
-
-export function bishopPositions(x:number, y:number) {
-  const bishopPositions = [
-    { x: x + 1, y: y + 1 },
-    { x: x + 1, y: y - 1 },
-    { x: x + 2, y: y + 2 },
-    { x: x + 2, y: y - 2 },
-    { x: x + 3, y: y + 3 },
-    { x: x + 3, y: y - 3 },
-    { x: x + 4, y: y + 4 },
-    { x: x + 4, y: y - 4 },
-    { x: x + 5, y: y + 5 },
-    { x: x + 5, y: y - 5 },
-    { x: x + 6, y: y + 6 },
-    { x: x + 6, y: y - 6 },
-    { x: x + 7, y: y + 7 },
-    { x: x + 7, y: y - 7 },
-
-    { x: x - 1, y: y + 1 },
-    { x: x - 1, y: y - 1 },
-    { x: x - 2, y: y + 2 },
-    { x: x - 2, y: y - 2 },
-    { x: x - 3, y: y + 3 },
-    { x: x - 3, y: y - 3 },
-    { x: x - 4, y: y + 4 },
-    { x: x - 4, y: y - 4 },
-    { x: x - 5, y: y + 5 },
-    { x: x - 5, y: y - 5 },
-    { x: x - 6, y: y + 6 },
-    { x: x - 6, y: y - 6 },
-    { x: x - 7, y: y + 7 },
-    { x: x - 7, y: y - 7 },
-  ];
-  const filteredBishopPositions = bishopPositions.filter(
-    (pos) => pos.x >= 0 && pos.y >= 0 && pos.x < 8 && pos.y < 8
-  );
-  return filteredBishopPositions;
+export function posiblePositionsByDirection(fromPosition:Iposition, boardGrid:(null|Piece)[][], directions:Iposition[], range:number){
+  const posiblePOsition:Iposition[]=[]
+  const myColor = boardGrid[fromPosition.y][fromPosition.x].color
+  directions.forEach((d)=>{
+      for (let j = 0; j < range; j++) {
+          const x = fromPosition.x + d.x*j;
+          const y = fromPosition.y + d.y*j;
+          const pos = {x,y};
+          const conditions = pos.x >= 0 && pos.x < boardGrid[0].length && pos.y >=0 && pos.y < boardGrid.length;
+          let stopOnNext =false
+          if (!conditions) return
+          if (boardGrid[pos.y][pos.x] !== null) {
+            if(boardGrid[pos.y][pos.x].color !== myColor) {
+              stopOnNext = true
+            } else {return}
+          };
+          posiblePOsition.push(pos);
+          if (stopOnNext) return
+      }
+  })
+  return posiblePOsition
 }
